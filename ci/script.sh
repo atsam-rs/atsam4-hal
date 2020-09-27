a9054bfd5dd8e785a2d19673711b36aa4cbeafdd
John W. Terrell <john@coolpeoplenetworks.com>
2020-09-27 11:20:14 -0700

    Updated travis to properly set features on build targets.

--- ci/script.sh
+++ ci/script.sh
@@ -1,7 +1,7 @@
 set -ex
 
 main() {
-    cargo check --target $TARGET
+    cargo check --target $TARGET --features $FEATURES
 
     if [ $TARGET = x86_64-unknown-linux-gnu ]; then
         cargo clean
