fb3ce5c2a8a0545524db4c9cb954a61e9ab71325
John W. Terrell <john@coolpeoplenetworks.com>
2021-04-16 19:38:23 -0700

    WIP

--- src/ethernet_controller/mii/bmcr.rs
+++ src/ethernet_controller/mii/bmcr.rs
@@ -1,8 +1,66 @@
+pub struct Reader {
+    pub(crate) bits: u16,
+}
+impl Reader {
+    pub(crate) fn new(bits: u16) -> Self {
+        Self {
+            bits,
+        }
+    }
+
+    pub fn bits(&self) -> u16 {
+        self.bits
+    }
+}
+impl<FI> PartialEq<FI> for R
+where
+    FI: Copy + Into<u16>,
+{
+    fn eq(&self, other: &FI) -> bool {
+        self.bits.eq(&(*other).into())
+    }
+}
+
+pub struct Writer {
+    pub(crate) bits: u16,
+}
+impl Writer {
+    pub fn bits(&mut self, bits: U) -> &mut Self {
+        self.bits = bits;
+        self
+    }
+}
+
 pub struct BMCR(u16);
 
 impl BMCR {
     pub fn new() -> Self {
         BMCR(0)
+    }
+
+    pub fn modify<F>(&self, f: F)
+    where
+        for<'w> F: FnOnce(&Reader, &'w mut Writer) -> &'w mut Writer,
+    {
+        let bits = self.get();
+        self.set(
+            f(
+                &Reader {
+                    bits,
+                },
+                &mut Writer {
+                    bits,
+                },
+            )
+            .bits,
+        );
+    }
+
+    fn set(&mut self, value: u16) {
+    }
+
+    fn get(&self) -> u16 {
+        0
     }
 
     pub fn set_speed_1000(self) -> Self {
